37948579f9003392532d12ddea58432f8e777dcb
Daniel Gross <daniel@dgross.ca>
2023-11-16 12:54:15 -0500

    Remove install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-wget "https://github.com/twpayne/chezmoi/releases/download/v2.40.4/chezmoi_2.40.4_linux_amd64.deb" -O /tmp/chezmoi.deb
-sudo dpkg -i /tmp/chezmoi.deb
-chezmoi init --apply danielpgross
